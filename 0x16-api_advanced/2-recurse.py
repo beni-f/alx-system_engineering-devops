d522e165836d69b4e5c83cf0ae7225bf270a9f9f
beni-f <benifissha@gmail.com>
2024-02-14 02:26:27 +0300

    updated

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-
 """
-    Recursive function that queries the Reddit API and returns a list
-    containing the titles of all hot articles for a given subreddit.
+    2-recurse.py
 """
 
 import requests
+import sys
 
-
-def recurse(subreddit, hot_list=[], after=""):
-    """Querying Reddit API, and returns all
-    hot articles for a given subreddit."""
-
+def get_hot_articles(subreddit, after=None):
+    """Helper function to fetch hot articles for a subreddit."""
     url = "https://api.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+
     if after:
         url += "?after={}".format(after)
 
@@ -24,10 +21,25 @@ def recurse(subreddit, hot_list=[], after=""):
         if "error" in data:
             return None
         children = data['data']['children']
-        for child in children:
-            hot_list.append(child['data']['title'])
+        titles = [child['data']['title'] for child in children]
         after = data['data']['after']
-        if after is not None:
-            return recurse(subreddit, hot_list, after)
-        else:
-            return hot_list
+        if after:
+            titles += get_hot_articles(subreddit, after)
+        return titles
+
+def recurse(subreddit, hot_list=[]):
+    """Recursively fetch hot articles for a subreddit."""
+    if get_hot_articles(subreddit):
+        hot_list += get_hot_articles(subreddit)
+        return hot_list
+    return None
+if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+else:
+    result = recurse(sys.argv[1])
+    if result is not None:
+        print(len(result))
+    else:
+        print("None")
+
+
