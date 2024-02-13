816d5f11c71c0bab3e99fb017443f97a199989b1
beni-f <benifissha@gmail.com>
2024-02-13 23:32:20 +0300

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,7 +11,7 @@ def number_of_subscribers(subreddit):
     """
     url = "https://reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Beni Fissha'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subs = data.get('data').get('subscribers')
