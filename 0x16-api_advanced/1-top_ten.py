db6e2c07019ac126ea8e82ad49b6df6dcefcba80
beni-f <benifissha@gmail.com>
2024-02-13 22:31:21 +0300

    initial commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    1-top_ten.py
+"""
+import requests
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+
+        for i, post in enumerate(posts[:10]):
+            print('{}'.format(post['data']['title']))
