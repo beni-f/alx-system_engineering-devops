4b59035b5ae0e1648351924f5916cef9ca373159
beni-f <benifissha@gmail.com>
2024-02-14 03:37:31 +0300

    updated

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,12 +8,13 @@ import requests
 
 def recurse(subreddit, hot_list=[], after=None):
     url = "https://api.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0\
+                (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,\
+                like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
     if after:
         url += "?after={}".format(after)
 
-    
     response = requests.get(url, headers=headers)
     data = response.json()
     if response.status_code == 200:
