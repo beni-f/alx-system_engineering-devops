9157ee163f8c0fb96a8785d60e42a5a932547c6f
beni-f <benifissha@gmail.com>
2024-04-09 20:57:51 +0300

    updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,15 +7,15 @@
 import requests
 
 
-
 def number_of_subscribers(subreddit):
     """
        Returns the number of subscribers for a given reddit
     """
-    url = f'https://api.reddit.com/r/{subreddit}/about'
-    headers = {'User-Agent': 'MyBot/0.1'}
-    resp = requests.get(url, headers=headers)
-    if resp.status_code == 200:
+    try:
+        url = f'https://api.reddit.com/r/{subreddit}/about'
+        headers = {'User-Agent': 'MyBot/0.1'}
+        resp = requests.get(url, headers=headers)
         data = resp.json()
-        return data['data'].get('subscribers', 0)
-    return 0
+        return data['data'].get('subscribers')
+    except:
+        return 0
