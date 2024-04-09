28956869674520a4d6fb96a9ad4a95d7a5c93350
beni-f <benifissha@gmail.com>
2024-04-09 20:39:29 +0300

    initial commit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+import requests
+"""
+0-subs.py
+"""
+def number_of_subscribers(subreddit):
+    """
+        A function that queries the Reddit API and returns the number of subscribers for a given reddit
+    """
+    url = f'https://api.reddit.com/r/{subreddit}/about'
+    headers = {'User-Agent': 'MyBot/0.1'}
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        data = resp.json()
+        return data['data'].get('subscribers', 0)
+    return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,2 @@
+# API ADVANCED
+## Working on Reddit API which provides alot of endpoints available that could help me learn more advanced api calls that require recursive functions and format/sort the results.
